b770b0aa57d47eb48c2847f32966be3cf69a9778
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-10 22:23:49 +0530

    Include an expanded selection of fonts
    Change-Id: I2713f0c939825762f1d724a9e45a028c99e84dc3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,3 +109,6 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
