cf54d4677587bef4f82ad69fbf0de7c588c2f60b
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-12 17:07:03 +0530

    Update media_codecs for L
    Change-Id: I8d6aa6e94c997c580e027b69ca137a6db1163774

--- msm8226-common.mk
+++ msm8226-common.mk
@@ -67,6 +67,9 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/media_codecs.xml:system/etc/media_codecs.xml
 
 # Display
