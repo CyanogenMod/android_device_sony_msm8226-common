4d0c278735c718a097de24e7a8cafe3b34ecc365
Adam Farden <adam@farden.cz>
2015-03-06 23:21:39 +0100

    yukon: move bootrec-device to root of device repo
    Change-Id: Ia02523b06fceff3b229eaccf464f6b4bbdf9381a

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -5,7 +5,7 @@ $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
 
 INITSH := device/sony/msm8226-common/boot/init.sh
-BOOTREC_DEVICE := device/sony/msm8226-common/boot/bootrec-device
+BOOTREC_DEVICE := device/sony/$(TARGET_DEVICE)/bootrec-device
 
 ## Build and run dtbtool
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
